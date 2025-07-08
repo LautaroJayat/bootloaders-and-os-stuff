27533e3864e05caa4c4bcd5b08baccd6d0378c64
Lautaro Jayat <lautaro.jayat@gmail.com>
2025-07-08 16:01:16 -0300

    feat: adding simple grub experiment

---
+++ 05_simplest_grub/src/boot.asm
@@ -0,0 +1,25 @@
+BITS 32
+
+section .text
+    ALIGN 4
+    DD 0x1BADB002
+    DD 0X00000000
+    DD -(0x1BADB002 + 0X00000000)
+
+global start
+extern kmain
+
+start:
+    CLI
+    mov esp, stack_space
+    CALL kmain
+    HLT
+haltKernel
+    CLI
+    HLT
+    jmp haltKernel
+
+section .bss
+RESB 8192
+
+stack_space:
